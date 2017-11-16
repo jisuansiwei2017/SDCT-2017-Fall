23aaba455746208f8bf4c792ece97142ad74cd12
Wang Shuyang <wangsy16@mails.tsinghua.edu.cn>
2017-11-16 10:19:32 +0800

    mult project04

---
+++ src/group5/project04_wangshuyang/mult.asm
@@ -0,0 +1,29 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+@0
+D=A
+@R2
+M=D
+(LOOPSTART)
+@R1
+MD=M-1
+@LOOPEND
+D;JLT
+@R0
+D=M
+@R2
+M=D+M
+@LOOPSTART
+0;JMP
+(LOOPEND)
+
+(INFINITELOOP)
+@INFINITELOOP
+0;JMP
